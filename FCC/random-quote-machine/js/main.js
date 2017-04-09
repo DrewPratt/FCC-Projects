d452ee33770adcb8c3da4aae8630878b4f4c2610
Gains <drewpratt@Drews-MacBook-Pro.local>
2017-04-09 16:04:06 -0700

    Random quote machine
    rebuilt without bootstrap and made to pass Free code camp test
    requirements

---
+++ FCC/random-quote-machine/js/main.js
@@ -0,0 +1,37 @@
+let func = {
+	colorChange: function () {
+		var tests = document.querySelectorAll(".colors");
+		var bods = document.getElementsByTagName('body');
+		var colors = ['#f44336' /*brightred*/ , '#c62828' /*darkred*/ , '#4CAF50' /*brightgreen*/ , '#2E7D32' /*darkgreen*/ , '#2196F3' /*brightblue*/ , '#1565C0' /*darkblue*/ , '#9C27B0' /*brightpurple*/ , '#6A1B9A' /*darkpurple*/ , '#FF9800' /*brightorange*/ , '#EF6C00' /*darkorange*/ , '#607D8B' /*bluegrey*/ , '#37474F' /*darkbluegrey*/ ];
+		var cols = colors[Math.floor(Math.random() * colors.length)];
+		bods[0].style.backgroundColor = cols;
+		tests[0].style.color = cols;
+		tests[1].style.color = cols;
+		console.log(tests);
+	},
+	getQuote: function () {
+		$.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/random", function (json) {
+			//			console.log(json);
+			$('#text').html(JSON.stringify(json.quote).replace(/[;]/g, ','));
+			$('#author').html(JSON.stringify("- " + json.author).replace(/["]/g, ''));
+		});
+	}
+}
+$(document).ready(function () {
+	func.colorChange();
+	func.getQuote();
+
+	$("#new-quote").on("click", function () {
+		// Only change code below this line.
+		func.colorChange();
+		func.getQuote();
+	});
+	$("#tweet-quote").on("click", function () {
+		var tweetQuote = document.getElementById("text").innerHTML + "  " + document.getElementById("author").innerHTML;
+		console.log(tweetQuote);
+		var a = document.getElementById("tweet-quote");
+		a.href = "https://twitter.com/intent/tweet?text=" + tweetQuote;
+	});
+
+
+})
